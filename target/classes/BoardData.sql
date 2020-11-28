66097035504d87d24ccd3c569e943f670305e0dc
taeyonghwang <xodyd15@naver.com>
2020-11-28 22:54:43 +0900

    설정 변경

---
+++ target/classes/BoardData.sql
@@ -0,0 +1,24 @@
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t1', 'c1', 'w1', 1234, '2020-11-28 22:00:00', 0);
+
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t2', 'c2', 'w1', 1234, '2020-11-28 22:00:00', 1);
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t3', 'c3', 'w1', 1234, '2020-11-28 22:00:00', 2);
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t4', 'c4', 'w1', 1234, '2020-11-28 22:00:00', 3);
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t5', 'c5', 'w1', 1234, '2020-11-28 22:00:00', 4);
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t6', 'c5', 'w1', 1234, '2020-11-28 22:00:00', 5);
+
+INSERT INTO BOARD (tile, content, writer, password, regDate, cnt)
+VALUES('t7', 'c5', 'w1', 1234, '2020-11-28 22:00:00', 6);
+
+
+
